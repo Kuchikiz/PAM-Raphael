1e5389571860e23ec5e80e4c660cf23f29d021a9
Kuchikiz <raphaelmorottimorotti@gmail.com>
2022-09-14 18:17:05 -0300

    projeto1409

---
+++ styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components/native';
+
+
+export const Container = styled.View`
+    flex:1;
+    backgroundColor: #fff;
+    alignItems: center;
+    justifyContent: center;
+`;
+
+export const Texto = styled.Text`
+    color: black;
+    font-size: 20px;
+    marginHorizontal: 15;
+    marginBottom: 10;
+`;
+
+export const Img = styled.Image`
+    width: 305;
+    height: 159;
+    marginBottom: 20;
+`;
